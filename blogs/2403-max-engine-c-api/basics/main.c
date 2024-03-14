759d93e4177d1c35d4f65f072e0b7eae4150ac3e
ehsanmok <6980212+ehsanmok@users.noreply.github.com>
2024-03-14 07:38:58 +0000

    Remove logs and use printf for simplicity

--- blogs/2403-max-engine-c-api/basics/main.c
+++ blogs/2403-max-engine-c-api/basics/main.c
@@ -3,41 +3,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-void logHelper(const char *level, const char *message, const char delimiter) {
-  printf("%s: %s%c", level, message, delimiter);
-}
-void logDebug(const char *message) { logHelper("DEBUG", message, ' '); }
-void logInfo(const char *message) { logHelper("INFO", message, '\n'); }
-void logError(const char *message) { logHelper("ERROR", message, '\n'); }
 
 int main() {
     const char *version = M_version();
-    logInfo("MAX Engine version:");
-    logInfo(version);
+    printf("MAX Engine version: %s\n", version);
 
     M_Status *status = M_newStatus();
     M_RuntimeConfig *runtimeConfig = M_newRuntimeConfig();
     M_RuntimeContext *context = M_newRuntimeContext(runtimeConfig, status);
     if (M_isError(status)) {
-        logError(M_getError(status));
+        printf("Error: %s\n", M_getError(status));
         return EXIT_FAILURE;
     }
-    logInfo("Context is setup");
+    printf("Context is setup\n");
 
     M_setNumThreads(runtimeConfig, 1);
     size_t numThreads = M_getNumThreads(runtimeConfig);
     // Buffer to hold the formatted string; ensure it's large enough
     char threadCountStr[50];
     snprintf(threadCountStr, sizeof(threadCountStr), "Number of threads: %zu", numThreads);
-    logInfo(threadCountStr);
+    printf("%s\n", threadCountStr);
 
     bool cpuAffinity = M_getCPUAffinity(runtimeConfig);
     char cpuAffinityStr[50];
     const char* affinityStatus = cpuAffinity ? "true" : "false";
     snprintf(cpuAffinityStr, sizeof(cpuAffinityStr), "CPU Affinity is set: %s", affinityStatus);
-    logInfo(cpuAffinityStr);
+    printf("%s\n", cpuAffinityStr);
 
     // free runtime resources
     M_freeRuntimeConfig(runtimeConfig);
